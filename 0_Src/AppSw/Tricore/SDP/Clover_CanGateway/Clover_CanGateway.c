7f7f377cfc3b9cd4119199ac9d13073a22b77489
Terrysyoon <yoonsb@hanyang.ac.kr>
2023-08-24 23:32:25 +0900

    WIP: Gateway Crash
    This commit aims to solve the issue described below:
    The dashboard is not able to receive the message from the steering.
    
    That is, the dashboard updates the testCount and increases success counter for the corresponding can message object but all of it's data is skewed. (It was all zero when I tested, so was the third message from VCU.)
    
    Its arriving rate is rather higher than expected and seems like it is somehow reading the third message from VCU, which ought to be read from the other CAN node.
    
    My theory is that something has crashed during the initializing process and the main suspect is that the iterating sequence of the for loops are somehow unexpectedly is altered during the compiler optimization.

--- 0_Src/AppSw/Tricore/SDP/Clover_CanGateway/Clover_CanGateway.c
+++ 0_Src/AppSw/Tricore/SDP/Clover_CanGateway/Clover_CanGateway.c
@@ -11,7 +11,9 @@ CanCommunication_Message DashboardToSteering_FROM_STEERING[_NUM_GATEWAY_MSG_FROM
 void Clover_CanGateway_init(){
 	const uint32 GatewayMsg_ID_FROM_VCU[_NUM_GATEWAY_MSG_FROM_VCU_] = {0x00101F00UL, 0x00101F01UL, 0x00101F02UL}; //StWhlMsgId1, StWhlMsgId2, StWhlMsgId3, StWhlRSWMsgId
 	const uint32 GatewayMsg_ID_FROM_STEERING[_NUM_GATEWAY_MSG_FROM_STEERING_] = {0x00101F10UL};
-	for(int i = 0; i < _NUM_GATEWAY_MSG_FROM_VCU_; i++) {
+	volatile int i;
+
+	for(i = 0; i < _NUM_GATEWAY_MSG_FROM_VCU_; i++) {
 		{
 	        CanCommunication_Message_Config config;
 	        config.messageId		=	GatewayMsg_ID_FROM_VCU[i];
@@ -29,7 +31,7 @@ void Clover_CanGateway_init(){
 	        CanCommunication_initMessage(&DashboardToSteering_FROM_VCU[i], &config);
 		}
 	}
-	for(int i = 0; i < _NUM_GATEWAY_MSG_FROM_STEERING_; i++) {
+	for(i = 0; i < _NUM_GATEWAY_MSG_FROM_STEERING_; i++) {
 		{
 	        CanCommunication_Message_Config config;
 	        config.messageId		=	GatewayMsg_ID_FROM_STEERING[i];
@@ -53,14 +55,15 @@ uint32 tick_1ms = 0;
 uint32 tick_10ms = 0;
 
 void Clover_CanGateway_run_1ms() { //10ms
-	for(int i = 0; i < _NUM_GATEWAY_MSG_FROM_VCU_; i++) {
+	volatile int i;
+	for(i = 0; i < _NUM_GATEWAY_MSG_FROM_VCU_; i++) {
 	    if(CanCommunication_receiveMessage(&VCUtoDashboard_FROM_VCU[i]))
 	    {
 	    	CanCommunication_setMessageData(VCUtoDashboard_FROM_VCU[i].msg.data[0],VCUtoDashboard_FROM_VCU[i].msg.data[1],&DashboardToSteering_FROM_VCU[i]);
 	    }
 	    //CanCommunication_transmitMessage(&DashboardToSteering[i]);
 	}
-	for(int i = 0; i < _NUM_GATEWAY_MSG_FROM_STEERING_; i++) {
+	for(i = 0; i < _NUM_GATEWAY_MSG_FROM_STEERING_; i++) {
 	    if(CanCommunication_receiveMessage(&VCUtoDashboard_FROM_STEERING[i]))
 	    {
 	    	CanCommunication_setMessageData(VCUtoDashboard_FROM_STEERING[i].msg.data[0],VCUtoDashboard_FROM_STEERING[i].msg.data[1],&DashboardToSteering_FROM_STEERING[i]);
@@ -71,10 +74,11 @@ void Clover_CanGateway_run_1ms() { //10ms
 }
 
 void Clover_CanGateway_run_10ms() { //10ms
-	for(int i = 0; i < _NUM_GATEWAY_MSG_FROM_VCU_; i++) {
+	volatile int i;
+	for(i = 0; i < _NUM_GATEWAY_MSG_FROM_VCU_; i++) {
 	    CanCommunication_transmitMessage(&DashboardToSteering_FROM_VCU[i]);
 	}
-	for(int i = 0; i < _NUM_GATEWAY_MSG_FROM_STEERING_; i++) {
+	for(i = 0; i < _NUM_GATEWAY_MSG_FROM_STEERING_; i++) {
 	    CanCommunication_transmitMessage(&DashboardToSteering_FROM_STEERING[i]);
 	}
 	tick_10ms++;
